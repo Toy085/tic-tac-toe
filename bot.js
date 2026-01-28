ac84735009d5f2db32b7b902b675380193fac2f2
Toy085 <brightabaka1@gmail.com>
2026-01-28 17:06:32 +0100

    Updated bot script to use cases instead of if statments

--- bot.js
+++ bot.js
@@ -8,23 +8,11 @@ function botMove(gameState, botMode)
     const cornerTaken = corners.some(index => gameState[index] !== "");
     const edgeTaken = edges.some(index => gameState[index] !== "");
 
-    // Random mode
-    if (botMode === 1)
-    {
-        let wantToPlay = null;
-        while (true)
-        {
-            wantToPlay = Math.floor(Math.random() * 10)
-            if (gameState[wantToPlay] === "")
-            {
-                return wantToPlay;
+    switch (botMode) {
+        case 0:
+            if (cornerTaken && gameState[4] === "") {
+                return 4;
             }
-        }
-    }
-
-    if (cornerTaken && gameState[4] === "") {
-        return 4;
-    }
     /*if (edgeTaken) {
         for (let i = 0; i < edges.length; i++) {
             let edgeIndex = edges[i];
@@ -34,9 +22,25 @@ function botMove(gameState, botMode)
         }
     }*/
 
-    for (let i = 0; i < gameState.length; i++) {
-        if (gameState[i] === "") {
-            return i;
+            for (let i = 0; i < gameState.length; i++) {
+                if (gameState[i] === "") {
+                    return i;
+                }
+            }
+        break;
+    case 1:
+        break;
+    case 2:
+        break;
+    case 3:
+        let wantToPlay = null;
+        while (true)
+        {
+            wantToPlay = Math.floor(Math.random() * 10)
+            if (gameState[wantToPlay] === "")
+            {
+                return wantToPlay;
+            }
         }
     }
     return null;
